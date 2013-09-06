de44e1b8812a5a83ca397cf0c5dda380568e65e1
thethanghn <thethang85@gmail.com>
2013-09-06 18:00:02 +0700

    add missing files

---
+++ test/unit/enums/post_type_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PostType::StoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+
+class PostType::PoemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
