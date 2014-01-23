ace31117f927dbd53d6c7870f86c75d6855f3ad5
thethanghn <thethanghn@gmail.com>
2014-01-23 21:46:42 +0800

    to json

--- app/controllers/mobile/chapters_controller.rb
+++ app/controllers/mobile/chapters_controller.rb
@@ -1,5 +1,14 @@
 class Mobile::ChaptersController < Mobile::MobileController
   def index
-    @chapters = MangaReaderCrawler.chapterize(params[:manga_id])
+    manga = Manga.find(params[:manga_id]);
+    if manga.chapters.count == 0
+      @chapters = MangaReaderCrawler.chapterize(params[:manga_id])
+      
+      @chapters.each do |chapter|
+        manga.chapters << Chapter.new(chapter)
+      end
+    end
+
+    render json: manga.chapters.to_json
   end
 end
